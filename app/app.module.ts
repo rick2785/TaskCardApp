9d6b7401b96caac738d9e492ae1d4a1920a1b0e8
Rickey Hrabowskie <rickeyhrabowskie@Rickeys-MBP.att.net>
2017-02-12 12:24:45 -0600

    first commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { JsonpModule, HttpModule } from '@angular/http'; 
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { WeatherComponent } from './weather-widget/component/weather.component';
+
+import { SpeedUnitPipe } from './weather-widget/pipe/speed-unit.pipe';
+import { TempUnitPipe } from './weather-widget/pipe/temp-unit.pipe';
 
 @NgModule({
-    imports: [ BrowserModule ],
-    declarations: [ AppComponent ],
+    imports: [ 
+        BrowserModule,
+        FormsModule,
+        JsonpModule,
+        HttpModule
+        ],
+    declarations: [ 
+        AppComponent,
+        CardComponent,
+        WeatherComponent,
+        SpeedUnitPipe,
+        TempUnitPipe 
+        ],
     bootstrap: [ AppComponent ]
 })
 
